695797ab0feecbd1f62637b127f8d9420aa0be50
Eduardo Martimiano <eduardomartimiano@hotmail.com>
2023-05-24 18:20:10 -0300

    multiplayer_online

---
+++ Assets/Personagens/scripts/multiplayer_online/multiplayer_online.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+
+public class multiplayer_online : MonoBehaviourPun
+{
+    public float Speed;
+    public float acelerar;
+    public float rotateSpeed;
+    private Vector3 moveDirection = Vector3.zero;
+    public TMP_Text tempotxt;
+    float temposec;
+    float tempomin;
+    public Scrollbar velocidadeatual;
+    public Scrollbar drift;
+    float tempodrift;
+    bool turbo;
+    float tempoturbo;
+    float normalspeed;
+    public int pontodecontrole;
+    public TimerController controladortempo;
+    public GameObject vitoria;
+    public string tempovolta;
+    public TMP_Text tempodavolta;
+    public float gravity = 9.81f;
+    public PhotonView photonview;
+    public Camera myCamera;
+
+
+    void Start()
+    {
+        normalspeed = Speed;
+
+        velocidadeatual = GameObject.FindGameObjectWithTag("BARRA_VELOCIDADE").GetComponent<Scrollbar>();
+        drift = GameObject.FindGameObjectWithTag("BARRA_DRIFT").GetComponent<Scrollbar>();
+        tempotxt = GameObject.FindGameObjectWithTag("TXT_TEMPO").GetComponent<TMP_Text>();
+
+        photonview = GetComponent<PhotonView>();
+
+        if (!photonView.IsMine)
+        {
+            myCamera.gameObject.SetActive(false);
+        }
+
+        Debug.LogWarning("Name: " + PhotonNetwork.NickName + " PhotonView: " + photonview.IsMine);
+
+    }
+
+    void Update()
+    {
+        if (photonview.IsMine)
+        {
+
+            velocidadeatual.size = acelerar;
+            drift.size = tempodrift / 4;
+
+            temposec = temposec + Time.deltaTime;
+            if (temposec < 60)
+            {
+                if (temposec > 10)
+                {
+                    tempotxt.text = "Tempo: " + tempomin + ":" + temposec.ToString("0.00").Replace(',', ':');
+                }
+                else
+                {
+                    tempotxt.text = "Tempo: " + tempomin + ":0" + temposec.ToString("0.00").Replace(',', ':');
+                }
+            }
+            else
+            {
+                tempomin++;
+                temposec = 0;
+            }
+
+
+            CharacterController controller = GetComponent<CharacterController>();
+
+            if (Input.GetAxis("Fire2_2") == 1 && acelerar < 1)
+            {
+                acelerar = acelerar + 0.05f;
+            }
+            if (Input.GetAxis("Fire2_2") == 0 && acelerar > 0)
+            {
+                acelerar = acelerar - 0.01f;
+            }
+            if (acelerar < 0)
+            {
+                acelerar = 0;
+            }
+            moveDirection = new Vector3(0, -gravity, acelerar * -1 * Speed);
+            moveDirection = transform.TransformDirection(moveDirection);
+
+
+
+
+            if (Input.GetButtonDown("Fire1_2"))
+            {
+                Speed = Speed * 0.8f;
+                rotateSpeed *= 1.5f;
+            }
+            if (Input.GetButton("Fire1_2"))
+            {
+                tempodrift = tempodrift + Time.deltaTime;
+                if (tempodrift > 4)
+                {
+                    turbo = true;
+                }
+            }
+            if (Input.GetButtonUp("Fire1_2"))
+            {
+                tempodrift = 0;
+                rotateSpeed *= 0.66667f;
+                Speed = Speed * 1.25f;
+            }
+            if (tempodrift == 0 && turbo && tempoturbo < 3)
+            {
+                tempoturbo = tempoturbo + Time.deltaTime;
+                Speed = normalspeed * 2;
+            }
+            else if (tempoturbo >= 3)
+            {
+                Speed = normalspeed;
+                tempoturbo = 0;
+                turbo = false;
+            }
+
+            controller.Move(moveDirection * Time.deltaTime);
+
+            transform.Rotate(0, Input.GetAxis("Horizontal2") * rotateSpeed, 0);
+        }
+    }
+
+    public void contadortempo()
+    {
+        pontodecontrole++;
+        if (pontodecontrole == controladortempo.barreiras.Length)
+        {
+            vitoria.SetActive(true);
+            tempovolta = tempotxt.text;
+            tempodavolta.text = tempovolta;
+        }
+    }
+
+}
